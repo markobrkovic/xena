8f911685c52c21b0ca33d53ca820a85c34107a56
markobrkovic <querry999@gmail.com>
2021-11-25 15:39:58 +0100

    :sparkles: Add input component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,14 +1,14 @@
-import ButtonElement from './Button';
+import Button from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
-  component: ButtonElement,
+  component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 };
 
 export const Small = () => (
-  <ButtonElement
+  <Button
     text="Add to Wishlist"
     color="text"
     backgroundColor="quaternary-light"
@@ -17,7 +17,7 @@ export const Small = () => (
 );
 
 export const Medium = () => (
-  <ButtonElement
+  <Button
     text="Add to Cart"
     color="text--contrast"
     backgroundColor="tertiary"
@@ -26,10 +26,5 @@ export const Medium = () => (
 );
 
 export const Large = () => (
-  <ButtonElement
-    text="EXPLORE"
-    color="text"
-    backgroundColor="primary"
-    size="large"
-  />
+  <Button text="EXPLORE" color="text" backgroundColor="primary" size="large" />
 );
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-type ButtonElementProps = {
+type ButtonProps = {
   text: string;
   color: string;
   backgroundColor: string;
   size: string;
 };
 
-export default function ButtonElement({
+export default function Button({
   text,
   color,
   backgroundColor,
   size,
-}: ButtonElementProps) {
+}: ButtonProps) {
   return (
     <button
       className={`${styles.button} ${styles[color]} ${styles[backgroundColor]} ${styles[size]}`}
