aa1bea65836d18857593e752edfd7a0fff641f0d
markobrkovic <querry999@gmail.com>
2021-11-30 12:31:36 +0100

    Remove unnecessary code

--- src/utils/fetchAPI.tsx
+++
@@ -1,8 +0,0 @@
-export async function fetchGames() {
-  const response = await fetch(
-    'https://store.steampowered.com/api/appdetails?appids=730'
-  );
-  const body = await response.json;
-  console.log(body);
-  return body;
-}
