d0bc030850e446e4bc75a6e3a89854952686375f
markobrkovic <querry999@gmail.com>
2021-12-02 00:02:26 +0100

    Edit fetch API

--- src/utils/fetchAPI.ts
+++ src/utils/fetchAPI.ts
@@ -1,5 +1,5 @@
 export default async function fetchGameInfo() {
-  const response = await fetch('http://localhost:3001/api/twitchgames');
+  const response = await fetch('/api/twitchgames');
   const body = await response.json();
   console.log(body[0].screenshots);
   return body[0];
