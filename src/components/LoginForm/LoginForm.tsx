efdd1618ff95958c51f55af921909eff1cf09b1a
markobrkovic <querry999@gmail.com>
2021-12-19 15:59:10 +0100

    :lipstick: Update LoginForm CSS

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -4,6 +4,7 @@ import styles from './LoginForm.module.css';
 import { useState } from 'react';
 import userAuthAPI from '../../utils/userAuthAPI';
 import { useNavigate } from 'react-router-dom';
+import Line from '../design-components/Line/Line';
 
 export default function LoginForm() {
   const [username, setUserName] = useState<string>('');
@@ -19,57 +20,60 @@ export default function LoginForm() {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.formHeader}>Login to XENA </h1>
-      <form
-        className={styles.formLogin}
-        onSubmit={async (event) => {
-          event.preventDefault();
-          console.log({ username, password });
-          if (username != null && password != null) {
-            const auth = await userAuthAPI({ username, password });
-            console.log('Hello ' + auth.username);
-            setCookieFunction(auth.username);
-          } else {
-            console.log('Nope');
-          }
-        }}
-      >
-        <span>username</span>
-        <Input
-          onChange={setUserName}
-          className={styles.formLogin_input}
-          placeholder="Enter username"
-          color="text--contrast"
-          size="medium"
-          backgroundColor="background--contrast"
-        />
-        <span>password</span>
-        <Input
-          onChange={setPassword}
-          type="password"
-          className={styles.formLogin_input}
-          placeholder="Enter password"
-          color="text--contrast"
-          size="medium"
-          backgroundColor="background--contrast"
-        />
-        <Button
-          text="Login"
-          color="text"
-          backgroundColor="quaternary-light"
-          size="small"
-        />
-      </form>
-      <section className={styles.formRegister}>
-        <span>Don&apos;t have an account?</span>
+      <div className={styles.form}>
+        <h1 className={styles.formHeader}>Login to XENA </h1>
+        <Line width="viewport" highestOpacityPoint="middle" />
+        <form
+          className={styles.formLogin}
+          onSubmit={async (event) => {
+            event.preventDefault();
+            console.log({ username, password });
+            if (username != null && password != null) {
+              const auth = await userAuthAPI({ username, password });
+              console.log('Hello ' + auth.username);
+              setCookieFunction(auth.username);
+            } else {
+              console.log('Nope');
+            }
+          }}
+        >
+          <span>username</span>
+          <Input
+            onChange={setUserName}
+            className={styles.formLogin_input}
+            placeholder="Enter username"
+            color="text--contrast"
+            size="medium"
+            backgroundColor="background--contrast"
+          />
+          <span>password</span>
+          <Input
+            onChange={setPassword}
+            type="password"
+            className={styles.formLogin_input}
+            placeholder="Enter password"
+            color="text--contrast"
+            size="medium"
+            backgroundColor="background--contrast"
+          />
+          <Button
+            text="Login"
+            color="text"
+            backgroundColor="quaternary-light"
+            size="small"
+          />
+        </form>
+        <section className={styles.formRegister}>
+          <span>Don&apos;t have an account?</span>
 
-        <Button
-          text="Register"
-          color="text"
-          backgroundColor="quaternary-light"
-          size="small"
-        />
-      </section>
+          <Button
+            text="Register"
+            color="text"
+            backgroundColor="quaternary-light"
+            size="small"
+          />
+        </section>
+      </div>
     </div>
   );
 }
