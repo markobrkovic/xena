9f33efb202c42ebfb047b4711aef20a5fcd7cb2a
markobrkovic <querry999@gmail.com>
2021-12-19 13:41:52 +0100

    :sparkles: Add search input for friends

--- src/Pages/Friends/Friends.tsx
+++ src/Pages/Friends/Friends.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 import styles from './Friends.module.css';
 import fetchFriends from '../../utils/fetchFriends';
-import { useParams } from 'react-router-dom';
 import Navbar from '../../components/Navbar/Navbar';
 import Friend from '../../components/Friend/Friend';
+import SearchInput from '../../components/SearchInput/SearchInput';
 
 export type FriendProps = {
   username: string | null;
@@ -12,8 +12,9 @@ export type FriendProps = {
 
 export default function Friends() {
   const [friends, setFriends] = useState<null | FriendProps[]>(null);
-  const { id } = useParams();
+  const [search, setSearch] = useState<null | string>(null);
   const username = localStorage.getItem('username');
+  console.log(search);
   let content;
   useEffect(() => {
     async function getFriends() {
@@ -40,6 +41,12 @@ export default function Friends() {
     <>
       <div className={styles.container}>
         <Navbar title="Friends" />
+        <span className={styles.addOrSearch}>Add or Search For a Friend</span>
+        <SearchInput
+          text="Enter friend's username"
+          className={styles.searchInput}
+          onSearch={setSearch}
+        />
         {content}
       </div>
     </>
