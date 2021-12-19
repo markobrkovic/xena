f6cda66a15e4bdeb515f20e3e25799082f8bd464
markobrkovic <querry999@gmail.com>
2021-12-19 16:34:30 +0100

    :lipstick: Update Navbar and Dropdown CSS

--- src/components/Navbar/Navbar.stories.tsx
+++ src/components/Navbar/Navbar.stories.tsx
@@ -1,3 +1,4 @@
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -7,4 +8,10 @@ export default {
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 };
 
-export const NavbarOnWishlistPage = () => <Navbar title="Wishlist" />;
+export const NavbarOnWishlistPage = () => (
+  <MemoryRouter initialEntries={['/wishlist']}>
+    <Routes>
+      <Route path="/wishlist" element={<Navbar title="Wishlist" />} />
+    </Routes>
+  </MemoryRouter>
+);
